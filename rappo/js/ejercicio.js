3acfefda1ebb4e1c765166445af8f31baf6b4ead
leowipin <leonardopincayjaramillo@gmail.com>
2022-07-07 11:23:03 -0500

    change

--- rappo/js/ejercicio.js
+++ rappo/js/ejercicio.js
@@ -13,19 +13,35 @@ cargarDatos = () => {
                 let plantillaOption = `<option value = "${id}">${nombre}</option>`
                 let etiquetaSelect = document.querySelector('select')
                 etiquetaSelect.innerHTML += plantillaOption
-                etiquetaSelect.addEventListener('change', (event) => {
-                    fetch("https://dataserverdaw.herokuapp.com/escritores/frases")
-                        .then(response => response.json())
-                        .then(data => {
-                            data.frases.array.forEach(element => {
-                                console.log("sa")
-                            });
-                            
-                            
-                        })
-                        .catch(console.error);
-                })
             }
+            let etiquetaSelect = document.querySelector('select')
+            etiquetaSelect.addEventListener('change', (event) => {
+                fetch("https://dataserverdaw.herokuapp.com/escritores/frases")
+                    .then(response => response.json())
+                    .then(data => {
+                        let arrayFrases = data.frases
+                        let frases = document.getElementById('frases')
+                        frases.innerHTML = "";
+                        for (let i2 = 0; i2 < arrayFrases.length; i2++) {
+                            if (arrayFrases[i2].id_autor.toString() === etiquetaSelect.value) {
+                                let plantilla = `<div class="col-lg-3">
+                                                    <div class="test-inner ">
+                                                        <div class="test-author-thumb d-flex">
+                                                            <div class="test-author-info">
+                                                                <h4>${arrayFrases[i2].id_autor.toString()}</h4>                                            
+                                                            </div>
+                                                        </div>
+                                                        <span>${arrayFrases[i2].texto}</span>
+                                                        <i class="fa fa-quote-right"></i>
+                                                     </div>
+                                                </div>`
+                                frases = document.getElementById('frases')
+                                frases.innerHTML += plantilla
+                            }
+                        }
+                    })
+                    .catch(console.error);
+            })
 
         })
         .catch(console.error);
